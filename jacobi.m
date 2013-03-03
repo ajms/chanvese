73892d9a76c45ec88b2010621d9dff6dac830262
ajms <albert.schulz@gmail.com>
2013-03-03 17:34:38 +0100

    Using matlab features for inner loop.

--- jacobi.m
+++ jacobi.m
@@ -8,12 +8,20 @@ x = ones(n,1);
 u = zeros(n,1);
 error = 1;
 k = 0;
-while error > 10^(-6) && k < M
-    for i=1:n
-        j = 1:n;
-        j(i) = [];
-        u(i) = (b(i)-sum(A(i,j)*x(j)))/A(i,i);
+
+for i = 1:n
+    j = 1:n;
+    j(i) = [];
+    B = abs(A(i,j));
+    Check(i) = abs(A(i,i)) - sum(B); % Is the diagonal value greater than the remaining row values combined?
+    if Check(i) < 0
+        fprintf('The matrix is not strictly diagonally dominant at row %2i\n\n',i)
     end
+end
+
+while error > 10^(-2) && k < M
+    Ax = bsxfun(@times,A,reshape(x,1,n));
+    u = (b-sum(Ax-diag(diag(Ax),0),2))./diag(A);
     error = norm(u-x,2);
     x = u;
     k = k+1;
