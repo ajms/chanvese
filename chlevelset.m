1759f9e3035ec8806e13eb362e622aaa31db6113
ajms <albert.schulz@gmail.com>
2013-03-06 20:05:36 +0100

    Boundary conditions added, mistake in zeta corrected.

--- chlevelset.m
+++ chlevelset.m
@@ -10,29 +10,18 @@ function [ phi_nn ] = chlevelset( phi_n, u0, lambda1, lambda2, mu, nu, dt, h )
 m = size(phi_n,1)-2;
 
 % zeta and xi determine the gradient of phi.
-zeta = sqrt(((phi_n(3:end, 2:end-1) - phi_n(2:end-1,2:end-1)).^2 + (phi_n(2:end-1,3:end)-phi_n(2:end-1,1:end-2)).^2)/h^2);
+zeta = sqrt(((phi_n(3:end, 2:end-1) - phi_n(2:end-1,2:end-1)).^2/h^2 + (phi_n(2:end-1,3:end)-phi_n(2:end-1,1:end-2)).^2)/(2*h)^2);
 
-xi = sqrt(((phi_n(2:end-1,3:end)-phi_n(2:end-1,2:end-1)).^2+(phi_n(3:end,2:end-1)-phi_n(1:end-2,2:end-1)).^2)/h^2);
+xi = sqrt(((phi_n(2:end-1,3:end)-phi_n(2:end-1,2:end-1)).^2/h^2+(phi_n(3:end,2:end-1)-phi_n(1:end-2,2:end-1)).^2)/(2*h)^2);
 
-
-% check ./ vs. /
 % C,D,E are the entries of the matrix for the system to solve.
 C = reshape(drac(phi_n(2:end-1,2:end-1),h)*mu./(h^2*xi),1,m^2); % y
 D = reshape(drac(phi_n(2:end-1,2:end-1),h)*mu./(h^2*zeta),1,m^2); % x
 E = reshape(drac(phi_n(2:end-1,2:end-1),h)*(-2*mu)./(h^2*(xi-zeta))-1/dt,1,m^2);
 
-fprintf('%d\n',size(C,2),size(E,2),size(D,2))
-
 % proper formulation for sparse matrices.
 % A denotes the matrix to solve.
 A = sparse(diag(E(1:end),0)) + sparse(diag(D(1:end-1),1)) + sparse(diag(D(2:end),-1)) + sparse(diag(C(m+1:end),-m)) + sparse(diag(C(1:end-m),m));
-
-% missing proper boundaries
-% Boundary conditions.
-gl = 1;
-gr = 1;
-gt = 1;
-gb = 1;
 
 % Build result vector from area, interior, exterior terms and boundary.
 u0L = u0(2:end-1,2:end-1);
@@ -40,14 +29,18 @@ u0L = u0(2:end-1,2:end-1);
 
 b = reshape(phi_n(2:end-1,2:end-1),m^2,1)/dt + nu*ones(m^2,1) + lambda1*(reshape(u0L,m^2,1)-c1).^2 - lambda2*(reshape(u0L,m^2,1)-c2).^2;
 
-for j=1:m
-    b(j) = b(j) + C(j)*gb;
-    b(end-j) = b(end-j) + C(end-j)*gt;
-    b(j*m) = b(j*m) + D(j*m)*gr;
-    b((j-1)*m+1) = b((j-1)*m+1) + D((j-1)*m+1)*gl;
-    if j > 1
-        A((j-1)*m,(j-1)*(m+1)) = 0;
-        A((j-1)*(m+1),(j-1)*m) = 0;
+for k=0:m-1
+    A(k*m+1,k*m+1) = A(k*m+1,k*m+1) + 4/3 * D(k*m+1); % Left boundary:
+    A(k*m+1,k*m+2) = A(k*m+1,k*m+2) - 1/3 * D(k*m+1); % Forward approximation
+    A((k+1)*m,(k+1)*m) = A((k+1)*m,(k+1)*m) - 4/3 * D((k+1)*m); % Right boundary:
+    A((k+1)*m,(k+1)*m-1) = A((k+1)*m,(k+1)*m-1) + 1/3 * D((k+1)*m); % Backward approximation
+    A(k+1,k+1) = A(k+1,k+1) + 4/3 * C((k+1)*m); % Buttom bounary:
+    A(k+2,k+1) = A(k+2,k+1) - 1/3 * C((k+1)*m); % Forward approximation
+    A(end-k,end-k) = A(end-k,end-k) - 4/3 * C(end-k); % Top boundary
+    A(end-(k+1),end-k) = A(end-(k+1),end-k) + 1/3 * C(end-k); % Backward approximation
+    if k > 0
+        A(k*m,k*(m+1)) = 0;
+        A(k*(m+1),k*m) = 0;
     end
 end
 
