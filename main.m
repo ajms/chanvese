06b9f750ac8a313bb6d4042ee4d839c466c3e4b0
ajms <albert.schulz@gmail.com>
2013-05-03 09:39:17 +0200

    Corrected regularization.

--- main.m
+++ main.m
@@ -1,20 +1,17 @@
 clear all;
 close all;
 
-[FileName, PathName, FilterIndex] = uigetfile('*', 'Select image');
-filename = strcat(PathName, FileName);
-[pathstr, name, ext] = fileparts(filename);
 
-if strcmpi(ext, '.mat')
-    S = load(filename);
-    u0 = S.I;
-else
-    RGB = imread(filename);
-    u0 = double(rgb2gray(RGB));
-end
 
-%RGB = imread('/home/albert/Dropbox/Uni/ProjNat/LevelSet/test_images/test2.jpg');
-%u0 = double(rgb2gray(RGB));
+
+filename = '/home/albert/Dropbox/Uni/Bachelorprojekt/LOL/medical_images/images1.mat';
+
+S = load(filename);
+I = S.I;
+
+%filename = '/home/albert/Dropbox/Uni/Bachelorprojekt/LOL/test_images/test2.jpg';
+%RGB = imread(filename);
+%I = double(rgb2gray(RGB));
 
 figure('Position', [100 150 300 500])
 
@@ -23,30 +20,33 @@ h = 1.0;
 dt = 0.1;
 lambda1 = 1;
 lambda2 = 1;
-mu = 1;%0.05*255^2;
-nu = 0;
+mu = 0;%0.5*255^2;
+nu = 0;%200;
 doreinit = 0;
-[M, N] = size(u0);
+[M, N] = size(I);
 
-phi = -ones(M, N);
+phi = -ones(M+2, N+2);
 
 % Initialise phi_0 as a circle
-[X Y] = meshgrid(1:M);
+[X Y] = meshgrid(1:M+2);
 Z = (X-50).^2 + (Y-50).^2; 
-phi(Z <= 10^2) = 1;
+phi(Z <= 30^2) = 1;
 
 % Initialise phi_0 as a rectangle
 %phi(5:15, 5:15) = 1;
 
 % Initialize to a signed distance function
-phi = reinit(phi);
+phi = init(phi);
 tempdphi = 5000;
-for i=1:200
-    clf;
+tempphi = zeros(M+2,N+2);
+for i=1:600
+    fprintf('Iteration: %d\n',i);
+    phi_n = chlevelset(phi, I, lambda1, lambda2, mu, nu, dt, h);
+    
     subplot(2,1,1);
-    imagesc(u0);
+    imagesc(I);
     hold on;
-    [C H] = contour(phi, [0 0], 'r');
+    [C H] = contour(phi_n(2:end-1,2:end-1), [0 0], 'r');
     set(H, 'LineWidth', 3);
     axis tight;
     colormap gray;
@@ -55,25 +55,25 @@ for i=1:200
     hold off;
     
     subplot(2,1,2);
-    surf(1:M, 1:N, phi, 'EdgeColor', 'none');
+    surf(1:M, 1:N, phi_n(2:end-1,2:end-1), 'EdgeColor', 'none');
     axis tight;
     xlabel('x');
     ylabel('y');
     zlabel('\phi^n(x, y)');
-    
     pause(0.1);
-    fprintf('Iteration: %d\n',i);
-    phi_n = chlevelset(phi, u0, lambda1, lambda2, mu, nu, dt, h);
     
-    % Stopping condition
-    dphi = norm(phi_n - phi, Inf);
-    if dphi < 10*h || abs(dphi-tempdphi) < 0.1
+    % Stopping condition, print timesteps. Procent of max value
+    dphi = norm(phi_n - tempphi);
+    ddphi = abs(dphi-tempdphi);
+    fprintf('Difference in phi: %f, %f\n',dphi,ddphi);
+    if dphi < 0.1 ||  ddphi< 0.1*dt
         fprintf('Stopping @ iteration %d\n', i);
         break;
     end
-    phi = phi_n;
+    tempphi = phi_n;
+    phi = init(phi_n);
     tempdphi = dphi;
 end
 
-set(gcf, 'PaperPositionMode', 'auto');
-print('-f1', '-djpeg', strcat(name, '-contour.jpg'));
+%set(gcf, 'PaperPositionMode', 'auto');
+%print('-f1', '-djpeg', strcat(name, '-contour.jpg'));
