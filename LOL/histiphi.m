7be57a9fa7140a7b194bdb4664c413063d9cd65e
ajms <albert.schulz@gmail.com>
2013-05-07 09:28:47 +0200

    Wrong directory.

--- LOL/histiphi.m
+++
@@ -1,5 +0,0 @@
-function [ histo ] = histiphi(i,j,usmooth,phi,beta)
-
-histo = sum(sum(PW(i,0,usmooth).*PW(j,beta,phi)));
-
-end
--- LOL/lolevelset.m
+++
@@ -1,7 +0,0 @@
-function [ F ] = lolevelset(phi)
-%LOLEVELSET Summary of this function goes here
-%   Detailed explanation goes here
-    
-    
-end
-
