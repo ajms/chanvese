92da56e0c8336523864667f1b3e55e0181bfc9ab
ajms <albert.schulz@gmail.com>
2013-06-04 09:08:27 +0200

    Comments added.

--- avg_intensity.m
+++ avg_intensity.m
@@ -1,7 +1,15 @@
-function [ c1, c2 ] = avg_intensity ( phi, u0, h )
-%AVERAGE Summary of this function goes here
-%   Detailed explanation goes here
-    c1 = sum(sum(u0.*hside(phi, h)))/sum(sum(hside(phi, h)));
-    c2 = sum(sum(u0.*(1 - hside(phi, h))))/sum(sum(1 - hside(phi, h)));
+function [ c1, c2 ] = avg_intensity ( phi, I, h )
+% Function to compute the average intensities of the segments
+%
+%   [c1, c2]  = avg_intensity ( phi, u0, h ) returns the average
+%   intensity of the inner segment (c1) and the outer segment (c2)
+%
+%   Input:
+%   - phi: levelset function
+%   - I: image
+%   - h: space step size
+    
+    c1 = sum(sum(I.*hside(phi, h)))/sum(sum(hside(phi, h)));
+    c2 = sum(sum(I.*(1 - hside(phi, h))))/sum(sum(1 - hside(phi, h)));
 end
 
